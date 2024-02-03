e522a7cc69ff95b5c46fa5095e8537f628ae6c9d
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-02-03 17:25:48 -0500

    CRUD simple prueba 2

---
+++ CrudsinSQL/build/classes/CrudsinSQL/CrudsinSQL.java
@@ -0,0 +1,10 @@
+
+package CrudsinSQL;
+
+public class CrudsinSQL {
+    public static void main(String[] args) {
+        Formulario inicio = new Formulario();
+        inicio.setLocationRelativeTo(null);
+        inicio.setVisible(true);
+    }
+}
---
+++ CrudsinSQL/build/classes/CrudsinSQL/Persona.java
@@ -0,0 +1,58 @@
+
+package CrudsinSQL;
+
+public class Persona {
+    String codigo, nombre, apellido, edad, telefono;
+
+    public Persona(String codigo, String nombre, String apellido, String edad, String telefono) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+ 
+       
+}
