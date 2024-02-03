4e6c876557f3e1db589a1b1ee78752634502608b
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-02-03 17:39:03 -0500

    Primeros paso usando Interfaz gráfica de usuario (GUI) en este episodio:  Implementacion_De_Diferentes_Componentes_GUI

---
+++ GUI/src/implementacion_de_componentes_gui/Implementacion_De_Componentes_GUI.java
@@ -0,0 +1,12 @@
+package implementacion_de_componentes_gui;
+
+import Vistas.Progress_Bar;
+
+public class Implementacion_De_Componentes_GUI {
+
+    public static void main(String[] args) {
+        Progress_Bar Loader = new Progress_Bar();
+        Loader.setVisible(true);
+    }
+
+}
