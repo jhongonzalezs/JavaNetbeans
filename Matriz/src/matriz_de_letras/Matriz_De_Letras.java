11804caf06d947ec92eb11fae80fe9cba7871be1
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-02-03 17:45:34 -0500

    Primos paso implementando sistemas 2x2,3x3,3x2,etc. En este episodio: Matriz_De_Letras.

---
+++ Matriz/src/matriz_de_letras/Matriz_De_Letras.java
@@ -0,0 +1,15 @@
+package matriz_de_letras;
+
+public class Matriz_De_Letras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio sig=new Inicio();
+        sig.setVisible(true);
+        
+    }
+    
+}
