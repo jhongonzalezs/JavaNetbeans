22f008cdb2431fbf7ce81e65245f725eed302771
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-02-03 17:23:02 -0500

    CRUD simple prueba 1

---
+++ CRUD_Nuevo/src/crud_nuevo/CRUD_Nuevo.java
@@ -0,0 +1,11 @@
+
+package crud_nuevo;
+
+
+public class CRUD_Nuevo {
+   public static void main(String[] args) {
+        Formulario inicio = new Formulario();
+        inicio.setLocationRelativeTo(null);
+        inicio.setVisible(true);
+    }
+}
