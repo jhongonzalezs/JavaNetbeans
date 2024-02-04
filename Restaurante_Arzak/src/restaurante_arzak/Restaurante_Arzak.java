5cda2d2feb8d92aacb062327354f03f24f5f252c
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-02-03 20:59:29 -0500

    CRUD simple para un restaurante local para ventas pequeñas.

---
+++ Restaurante_Arzak/src/restaurante_arzak/Restaurante_Arzak.java
@@ -0,0 +1,11 @@
+package restaurante_arzak;
+
+import vistas.Inicio;
+
+public class Restaurante_Arzak {
+    public static void main(String[] args) {
+        Inicio Principal = new Inicio();
+        Principal.setVisible(true);
+    }
+    
+}
