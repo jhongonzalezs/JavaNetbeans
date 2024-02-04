8c0bf9e847d474efec5c0953bb52a93fa3385e63
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-02-03 21:26:49 -0500

    Ejercicio basico de suma de matrices en la cual podemos sumar dos matrices de las mismas dimensiones (Cuadraticas) y arrojar una tercera matriz con los resultados.

---
+++ Suma_De_Matrices/src/suma_de_matrices/Suma_De_Matrices.java
@@ -0,0 +1,16 @@
+package suma_de_matrices;
+
+import java.lang.Math;
+import javax.swing.*;
+
+
+public class Suma_De_Matrices {
+
+
+    public static void main(String[] args) {
+        Config Inicio= new Config();
+        Inicio.setLocationRelativeTo(null);
+        Inicio.setVisible(true);
+    }
+    
+}
