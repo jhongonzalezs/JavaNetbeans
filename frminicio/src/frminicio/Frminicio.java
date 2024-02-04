03695a57854f77b5b48162adc412eeb32fd80fba
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-02-03 21:28:21 -0500

    Ejercicio basico de inicio de session  usando GUI

---
+++ frminicio/src/frminicio/Frminicio.java
@@ -0,0 +1,12 @@
+package frminicio;
+
+public class Frminicio {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        frmsesionU inicio = new frmsesionU();
+        inicio.setVisible(true);
+    }
+
+}
