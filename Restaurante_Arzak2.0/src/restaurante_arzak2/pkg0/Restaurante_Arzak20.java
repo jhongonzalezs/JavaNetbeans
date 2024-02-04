74514e8492f1c314bc6f6c24f020ceb87d38606e
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-02-03 21:05:20 -0500

    CRUD simple para un restaurante local para ventas pequeñas v2, algunos cambios en la GUI.

---
+++ Restaurante_Arzak2.0/src/restaurante_arzak2/pkg0/Restaurante_Arzak20.java
@@ -0,0 +1,11 @@
+package restaurante_arzak2.pkg0;
+
+public class Restaurante_Arzak20 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio In= new Inicio();
+        In.setVisible(true);
+    }
+    
+}
