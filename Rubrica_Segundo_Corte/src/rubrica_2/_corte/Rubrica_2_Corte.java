6f228a065eebcaeea63d489a49ea68c3b539dbf0
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-02-03 21:10:25 -0500

    Rubrica de algoritmo I realizada en la universidad de la costa.

---
+++ Rubrica_Segundo_Corte/src/rubrica_2/_corte/Rubrica_2_Corte.java
@@ -0,0 +1,13 @@
+package rubrica_2._corte;
+
+import vistas.Inicio;
+
+public class Rubrica_2_Corte {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio in = new Inicio();
+        in.setVisible(true);
+    }
+    
+}
